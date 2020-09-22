2c901b483ecb1fc385a2968caeeae9f08e8621c1
chengmeng <1951785709@qq.com>
2020-09-22 11:15:35 +0800

    配置中心

---
+++ cloud-config-center-3344/src/main/java/com/cm/example/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.cm.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description:
+ * @Author:cm
+ * @Date:2020/9/22 10:51
+ * @Version:1.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
