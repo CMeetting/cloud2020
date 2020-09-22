507bf97c63014ea1a9cb0688f7985039041fb8f8
chengmeng <1951785709@qq.com>
2020-09-22 17:43:35 +0800

    消息总线

---
+++ cloud-config-client-3355/src/main/java/com/cm/example/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.cm.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author:cm
+ * @Date:2020/9/22 11:34
+ * @Version:1.0
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
